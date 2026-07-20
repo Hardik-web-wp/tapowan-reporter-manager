03bd9457d3ae4685d00ef8c2bea832e21aa4baea
Hardik-web-wp <sambhavsandeshweb@gmail.com>
2026-07-20 16:30:53 +0530

    Add loader class

---
+++ includes/class-trm-loader.php
@@ -0,0 +1,16 @@
+<?php
+if (!defined('ABSPATH')) exit;
+
+class TRM_Loader {
+    private array $actions = [];
+
+    public function add_action(string $hook, $component, string $callback, int $priority = 10, int $accepted_args = 1): void {
+        $this->actions[] = compact('hook','component','callback','priority','accepted_args');
+    }
+
+    public function run(): void {
+        foreach ($this->actions as $action) {
+            add_action($action['hook'], [$action['component'], $action['callback']], $action['priority'], $action['accepted_args']);
+        }
+    }
+}
